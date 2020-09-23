9557264cb87fcaf115ff3e19412972e65de0b255
Andrea <andreabecsek@gmail.com>
2020-09-23 19:56:54 +0100

    add comments and format file

--- change-point/app.R
+++ change-point/app.R
@@ -1,21 +1,20 @@
-library(shiny)
-library(ggplot2)
-library(dplyr)
-library(magrittr)
-library(shinythemes)
-library(shinydashboard)
-library(zoo)
-library(DT)
-library(InspectChangepoint)
-
 # source global which contains helper functions, global variables and the data
 source("./global.R")
 
-# Define UI
+# Define User Interface
 ui <- dashboardPage(
   dashboardHeader(title = "Time series for given meter ID"),
   dashboardSidebar(
-    # select meter id to be plotted
+    checkboxGroupInput(
+      "cp_types", "Type of change point:",
+      c(
+        "Mean" = "mean",
+        "Trend" = "trend",
+        "Tracking" = "tracking"
+      )
+    ),
+
+    # Select meter id to be plotted
     selectInput(
       inputId = "meter_id",
       label = strong("Meter Id"),
@@ -23,29 +22,8 @@ ui <- dashboardPage(
       selected = "AW008"
     ),
 
-    checkboxInput(
-      inputId = "mean",
-      label = "Mean",
-      value = FALSE,
-      width = NULL
-    ),
 
-    checkboxInput(
-      inputId = "trend",
-      label = "Trend",
-      value = FALSE,
-      width = NULL
-    ),
-
-    checkboxInput(
-      inputId = "tracking",
-      label = "Tracking",
-      value = FALSE,
-      width = NULL
-    ),
-
-
-    # thresholds
+    # Thresholds for the change point detection algorithm
     sliderInput(
       inputId = "thresh1",
       label = strong("Threshold 1"),
@@ -74,6 +52,7 @@ ui <- dashboardPage(
       value = 2000
     ),
 
+    # Date range for the time series plot
     dateRangeInput(
       inputId = "meter_date",
       strong("Date range for meter"),
@@ -82,7 +61,8 @@ ui <- dashboardPage(
       min = "2017-01-01",
       max = "2020-01-26"
     ),
-    
+
+    # Date range for detecting change points
     dateRangeInput(
       inputId = "alerts_date",
       strong("Date range for alerts"),
@@ -91,34 +71,41 @@ ui <- dashboardPage(
       min = "2017-01-01",
       max = "2020-01-26"
     )
-
   ),
 
-  # main body
+  # Main Body
   dashboardBody(
     fluidRow(
-        box(
-          title = "ADF and MNF for chosen meter",
-          width = 7,
-          status = "primary",
-          plotOutput(outputId = "changepoints")
-        ),
-        valueBoxOutput("progressBox"),
-        
-          box(
-          width = 4,
-          title = "Alerts",
-          DT::dataTableOutput(outputId = "table"),style = "height:500px; overflow-y: scroll;overflow-x: scroll;"
-        )
+
+      # Box that contains the time series plot
+      box(
+        title = "ADF and MNF for chosen meter",
+        width = 7,
+        status = "primary",
+        plotOutput(outputId = "changepoints")
+      ),
+
+      # Box with the number of detected alerts
+      valueBoxOutput("progressBox"),
+
+      # Box that contains the table of dates and meter ids for the detected
+      # change points
+      box(
+        width = 4,
+        title = "Alerts",
+        DT::dataTableOutput(outputId = "table"),
+        style = "height:500px; overflow-y: scroll;overflow-x: scroll;"
       )
+    )
   )
 )
 
-# Define Server function
+# Define Server function (this is where the data is processed and outputs
+# are created)
 server <- function(input, output) {
   selected_series <- reactive({
 
-    # validate input data
+    # Validate input data
     req(input$meter_date)
     validate(need(
       !is.na(input$meter_date[1]) & !is.na(input$meter_date[2]),
@@ -129,120 +116,173 @@ server <- function(input, output) {
       "Error: Start date should be earlier than end date."
     ))
 
-    # filter data by given meter id and date range
+    # Filter data by given meter id and date range
     datFlow_ss_short %>%
       filter(id == input$meter_id) %>%
       filter(date >= as.Date(input$meter_date[1]) & date <= as.Date(input$meter_date[2]))
   })
 
-
-  # # start of jobs
-  # job_starts <- reactive({
-  #     alljobs %>%
-  #         filter(id == input$meter_id) %>%
-  #         filter(str_detect(key, "Start")) %>%
-  #         filter(as.Date(value) >= as.Date(input$date[1]), as.Date(value) <= as.Date(input$date[2]))
-  # })
-  #
-  # # end of jobs
-  # job_ends <- reactive({
-  #     alljobs %>%
-  #         filter(id == input$meter_id) %>%
-  #         filter(str_detect(key, "End")) %>%
-  #         filter(value >= as.Date(input$date[1]), value <= as.Date(input$date[2]))
-  # })
-
-
-  # differences = reactive({
-  #     data.frame(date = mnf_adf()$date,
-  #                diff = c(NA, diff(mnf_adf()$difference, lag = 1)))
-  # })
-  #
-  # output$differences = renderPlot({
-  #     #plot(diff(mnf_adf()$difference, lag=1), type='l')
-  #     ggplot(differences(), aes(x = as.Date(date), y= diff))+
-  #         geom_line()
-  # })
-
-
-  # # create plot object that the plotOutput function is expecting
-  # output$timeseries <- renderPlot({
-  #     plot <- ggplot(mnf_adf_1(), aes(x = as.Date(date), y = y, color = key)) +
-  #         geom_line() +
-  #         theme_bw() +
-  #         labs(x = "Date", y = "Flow")+
-  #         theme(legend.position = "none")
-  #
-  #     if (nrow(job_starts())) {
-  #         plot <- plot +
-  #             geom_vline(xintercept = as.Date(job_starts()$value), lty = 2)
-  #     }
-  #     if (nrow(job_ends())) {
-  #         plot <- plot + geom_vline(xintercept = as.Date(job_ends()$value), lty = 1)
-  #     }
-  #     plot
-  # })
-
-  output$timeseries <- renderPlot({
-    ggplot(selected_series()) +
-      geom_line(aes(x = as.Date(date), y = adf)) +
-      geom_line(aes(x = as.Date(date), y = mnf))+
-      theme_minimal()+
-      scale_color_manual(labels = c("psavert", "uempmed"), 
-                         values = c("psavert"="#00ba38", "uempmed"="#f8766d"))
+  # Based on input decide the types of change points to be detected
+  detect_mean <- reactive({
+    "mean" %in% input$cp_types
   })
 
-  # detect change points
+  detect_trend <- reactive({
+    "trend" %in% input$cp_types
+  })
+
+  detect_tracking <- reactive({
+    "tracking" %in% input$cp_types
+  })
+
+
+  # Compute a list of job starts for given id and date range
+  job_starts <- reactive({
+    alljobs %>%
+      filter(id == input$meter_id) %>%
+      filter(str_detect(key, "Start")) %>%
+      filter(
+        as.Date(value) >= as.Date(input$meter_date[1]),
+        as.Date(value) <= as.Date(input$meter_date[2])
+      )
+  })
+
+  # Compute a list of job ends for given id and date range
+  job_ends <- reactive({
+    alljobs %>%
+      filter(id == input$meter_id) %>%
+      filter(str_detect(key, "End")) %>%
+      filter(
+        as.Date(value) >= as.Date(input$meter_date[1]),
+        as.Date(value) <= as.Date(input$meter_date[2])
+      )
+  })
+
+  # output$timeseries <- renderPlot({
+  #   ggplot(selected_series()) +
+  #     geom_line(aes(x = as.Date(date), y = adf)) +
+  #     geom_line(aes(x = as.Date(date), y = mnf)) +
+  #     theme_minimal() +
+  #     scale_color_manual(
+  #       labels = c("psavert", "uempmed"),
+  #       values = c("psavert" = "#00ba38", "uempmed" = "#f8766d")
+  #     )
+  # })
+
+  # Detect change points for selected meter id
   change_points <- reactive({
     detect_changepoints(selected_series(),
-                        myid = input$meter_id,
-      inspect_thresh = c(input$thresh1, input$thresh2, input$thresh3, input$thresh4),
-      mean = input$mean,
-      trend = input$trend,
-      tracking = input$tracking
+      myid = input$meter_id,
+      inspect_thresh = c(
+        input$thresh1,
+        input$thresh2,
+        input$thresh3,
+        input$thresh4
+      ),
+      mean = detect_mean(),
+      trend = detect_trend(),
+      tracking = detect_tracking()
     )
   })
 
-  # add column of detected change points
+  # Add column of detected change points to make plotting easier
   dat_with_cp <- reactive({
     selected_series() %>%
       mutate(is_cp = ifelse(row_number() %in% change_points(), 1, 0))
   })
 
-  # plot change points
+  # Create time series plot of adf, mnf, job dates and change points
   output$changepoints <- renderPlot({
-    ggplot(dat_with_cp()) +
-      geom_line(aes(x = as.Date(date), y = adf_res, color="ADF")) +
-      geom_line(aes(x = as.Date(date), y = mnf_res, color="MNF")) +
+    plot <- ggplot() +
+      geom_line(
+        data = dat_with_cp(),
+        aes(
+          x = as.Date(date),
+          y = adf_res,
+          color = "ADF"
+        )
+      ) +
+      geom_line(
+        data = dat_with_cp(),
+        aes(
+          x = as.Date(date),
+          y = mnf_res,
+          color = "MNF"
+        )
+      ) +
       geom_vline(xintercept = dat_with_cp()$date[dat_with_cp()$is_cp == 1]) +
-      labs(x="Date",y="Flow",color="")+
-      theme_minimal()+
-      scale_color_manual(values = c("ADF"="#00ba38", "MNF"="#f8766d"))
-      #theme(legend.position = "bottom")
+      labs(
+        x = "Date",
+        y = "Flow",
+        color = ""
+      ) +
+      theme_minimal() +
+      scale_color_manual(values = c(
+        "ADF" = "#00ba38",
+        "MNF" = "#f8766d"
+      )) +
+      theme(aspect.ratio = 0.5)
+    
+    y_min = plot$coordinates$limits$y[1]
+    
+    no_start_jobs = length(as.Date(job_starts()$value))
+
+    # Add job start and end dates
+    if (nrow(job_starts())) {
+      plot <- plot +
+        # geom_vline(
+        #   xintercept = as.Date(job_starts()$value),
+        #   lty = 2, color = "red"
+        # )
+        geom_segment(aes(x=as.Date(job_starts()$value),
+                     xend=as.Date(job_starts()$value),
+                     y=rep(y_min,no_start_jobs),
+                     yend=rep(y_min,no_start_jobs)), 
+                     color='red')
+    }
+    if (nrow(job_ends())) {
+      plot <- plot +
+        geom_vline(
+          xintercept = as.Date(job_ends()$value),
+          lty = 2, color = "blue"
+        )
+    }
+    plot
   })
 
-  # # find all change points within given time range
-  all_cps = reactive({
-    detect_cps_within_dates(datFlow_ss_short,
-                            as.Date(input$alerts_date[1]),
-                            as.Date(input$alerts_date[2]))
+  # Find all change points within given time range
+  all_cps <- reactive({
+    detect_cps_within_dates(
+      datFlow_ss_short,
+      as.Date(input$alerts_date[1]),
+      as.Date(input$alerts_date[2])
+    )
   })
 
-  
-  # add table of alerts
-  output$table = renderDataTable({
+
+  # Add table of alerts with the detected change points
+  output$table <- renderDataTable({
     datatable(all_cps()$alerts)
   })
-  
-  # number of alerts
+
+  # Display the number of alerts
   output$progressBox <- renderValueBox({
     valueBox(
-      paste0(as.character(dim(all_cps()$alerts)[1]), " Alerts"), paste0("Between ",as.character(input$alerts_date[1])," and ", as.character(input$alerts_date[2])), icon = icon("exclamation-triangle"),
+      paste0(
+        as.character(dim(all_cps()$alerts)[1]),
+        " Alerts"
+      ),
+      paste0(
+        "Between ",
+        as.character(input$alerts_date[1]),
+        " and ",
+        as.character(input$alerts_date[2])
+      ),
+      icon = icon("exclamation-triangle"),
       color = "red"
     )
   })
-  
 }
 
-# create shiny object
+# Create shiny object
 shinyApp(ui = ui, server = server)
--- change-point/global.R
+++ change-point/global.R
@@ -1,3 +1,5 @@
+library(tidyr)
+library(stringr)
 library(shiny)
 library(ggplot2)
 library(dplyr)
@@ -5,36 +7,27 @@ library(magrittr)
 library(shinythemes)
 library(shinydashboard)
 library(zoo)
+library(DT)
+library(InspectChangepoint)
+library(grid)
 
 
-# Load Data for a single id and rename for ease
+# Load Data
 load("~/Documents/ww/datFlow_ss_short.RData")
-# datFlow = datFlow2
-# repFlow = repFlow2
-# dat <- datFlow_ss %>%
-#   filter(id == "AW070")
+load("~/Documents/ww/repFlow.Rda")
 
-# # function to compute the rolling mean and uses interpolation to impute NAs
-# inter_roll <- function(df, n_days = 7) {
-#   new <- df %>%
-#     mutate(value_roll = rollmean(value, n_days, na.pad = TRUE)) %>%
-#     select(-value)
-#   new$value_roll <- na.spline(new$value_roll, 1:nrow(new))
-#   return(new)
-# }
-# 
-# # All repair jobs
-# alljobs <- repFlow %>%
-#   select(-Region, -Area, -Area_reference) %>%
-#   gather("key", "value", -id) %>%
-#   na.omit()
+# All repair jobs
+alljobs <- repFlow %>%
+  select(-Region, -Area, -Area_reference) %>%
+  gather("key", "value", -id) %>%
+  na.omit()
 
-# helper function for transformng series
-compute_windowed_variance <- function(data, window_size){
+# Helper function for transformng series
+compute_windowed_variance <- function(data, window_size) {
   out <- numeric(length(data))
-  
-  for(i in 1:length(data)){
-    window_ind <- seq(max(1,i-window_size/2), min(length(data), i+window_size/2))
+
+  for (i in 1:length(data)) {
+    window_ind <- seq(max(1, i - window_size / 2), min(length(data), i + window_size / 2))
     out[i] <- var(data[window_ind])
   }
   return(out)
@@ -44,52 +37,54 @@ compute_windowed_variance <- function(data, window_size){
 # and a set of 4 thresholds, this function will output a set of times which contain changepoints
 # you can detect 3 types of changepoint: changes in the mean of adf or mnf, changes in the trend, and
 # you can also detect if adf and mnf are tracking eachother
-detect_changepoints <- function(data, myid, var_window = 20, inspect_thresh, mean=TRUE, trend=TRUE, tracking=TRUE){
+detect_changepoints <- function(data, myid, var_window = 20, inspect_thresh, mean = TRUE, trend = TRUE, tracking = TRUE) {
   data <- data %>% filter(id == myid)
-  
+
   series <- list(
     adf_res = scale(data$adf_res),
     mnf_res = scale(data$mnf_res),
-    transform1 = scale(compute_windowed_variance(data$adf_res-data$mnf_res, window_size = var_window)),
+    transform1 = scale(compute_windowed_variance(data$adf_res - data$mnf_res, window_size = var_window)),
     transform2 = scale(diff(data$adf_res))
   )
-  
+
   cp <- c()
-  if(mean == TRUE){
-    cp <- c(cp, inspect(series[[1]], thresh=inspect_thresh[1])$changepoints[,1])
-    cp <- c(cp, inspect(series[[2]], thresh=inspect_thresh[2])$changepoints[,1])
+  if (mean == TRUE) {
+    cp <- c(cp, inspect(series[[1]], thresh = inspect_thresh[1])$changepoints[, 1])
+    cp <- c(cp, inspect(series[[2]], thresh = inspect_thresh[2])$changepoints[, 1])
   }
-  if(tracking == TRUE){
-    cp <- c(cp, inspect(series[[3]], thresh=inspect_thresh[3])$changepoints[,1])
+  if (tracking == TRUE) {
+    cp <- c(cp, inspect(series[[3]], thresh = inspect_thresh[3])$changepoints[, 1])
   }
-  if(trend==TRUE){
-    cp <- c(cp, inspect(series[[4]], thresh=inspect_thresh[4])$changepoints[,1])
+  if (trend == TRUE) {
+    cp <- c(cp, inspect(series[[4]], thresh = inspect_thresh[4])$changepoints[, 1])
   }
-  
+
   return(cp)
 }
 
 
 
-# function that takes a date range and computes all the change points within that range
-detect_cps_within_dates = function(data, start_date, end_date){
+# Function that takes a date range and computes all the change points within that range
+detect_cps_within_dates <- function(data, start_date, end_date) {
   # keep only data from given date range
-  reduced_df = data %>% filter((as.Date(date) >= as.Date(start_date) & as.Date(date) <= as.Date(end_date)))
+  reduced_df <- data %>% filter((as.Date(date) >= as.Date(start_date) & as.Date(date) <= as.Date(end_date)))
   # create an empty matrix to store the detected change points for every meter
-  dates = reduced_df$date %>% unique()
-  ids = reduced_df$id %>% unique()
-  cps = matrix(rep(0, length(ids)*length(dates)),nrow = length(dates))
-  #compute change points
-  i = 1
+  dates <- reduced_df$date %>% unique()
+  ids <- reduced_df$id %>% unique()
+  cps <- matrix(rep(0, length(ids) * length(dates)), nrow = length(dates))
+  # compute change points
+  i <- 1
   for (id in ids) {
-    cp = detect_changepoints(reduced_df, myid = id, inspect_thresh = c(10,10,2000,2000))
-    cps[cp,i] = 1
-    i = i+1
+    cp <- detect_changepoints(reduced_df, myid = id, inspect_thresh = c(10, 10, 2000, 2000))
+    cps[cp, i] <- 1
+    i <- i + 1
   }
-  
-  #obtain matrix indeces where there is a changepoint
-  indices = which(cps==1, arr.ind = TRUE)
-  alerts = data.frame(date=dates[indices[,1]],
-                     id=ids[indices[,2]])
-  return(list(cps=cps, alerts=alerts))
+
+  # obtain matrix indeces where there is a changepoint
+  indices <- which(cps == 1, arr.ind = TRUE)
+  alerts <- data.frame(
+    date = dates[indices[, 1]],
+    id = ids[indices[, 2]]
+  )
+  return(list(cps = cps, alerts = alerts))
 }
